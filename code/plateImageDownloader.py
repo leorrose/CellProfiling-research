263214215e682f690d68574fbb2d52bf36763e6a
Naor Kolet <naorko@post.bgu.ac.il>
2021-08-30 21:41:02 +0300

    Download Images

---
+++ code/plateImageDownloader.py
@@ -0,0 +1,29 @@
+import os, sys
+from multiprocessing import Pool
+from urllib import request
+
+CHANNELS = ['Hoechst', 'ERSyto', 'ERSytoBleed', 'Ph_golgi', 'Mito']
+dest_path = '/storage/users/g-and-n/plates_imgs'
+
+def download_plate(plate):
+    plate_path = f'{dest_path}/{plate}'
+    os.makedirs(plate_path, exist_ok=True)
+    
+    for channel in CHANNELS:
+        url = f'http://cildata.crbs.ucsd.edu/broad_data/plate_{plate}/{plate}-{channel}.zip'
+        dest = f'{plate_path}/{plate}-{channel}.zip'
+        request.urlretrieve(url, dest)
+        
+        os.system(f'unzip -qj {dest_path}/{plate}/{plate}-{channel}.zip {plate}-{channel}/* -d {dest_path}/{plate}/')
+        os.system(f'rm {dest_path}/{plate}/{plate}-{channel}.zip')
+        
+    return None
+
+if __name__ == '__main__':
+    plate_numbers = sys.argv[1:]
+    
+    p = Pool(8)
+    p.map(download_plate, plate_numbers)
+    p.close()
+    p.join()
+    
