94b293e13065721456ee2c6b445b535c183fb3f5
Naor Kolet <naorko@post.bgu.ac.il>
2021-09-18 13:41:43 +0300

    Merge plate tabular downloaders

--- code/plateDownloader.py
+++ code/plateDownloader.py
@@ -2,7 +2,7 @@
 
 # Utils
 from sys import argv
-from os import path, scandir, makedirs, remove
+from os import path, scandir, makedirs, remove, listdir
 from tqdm import tqdm
 
 # Selector
@@ -28,13 +28,13 @@ from itertools import cycle
 # %% FTP Modifiable constants:
 
 FTP_LINK = r"parrot.genomics.cn/gigadb/pub/10.5524/100001_101000/100351"
-TIMEOUT = 10  # In seconds
-MAX_ATTEMPTS = 6  # Number of Retries upon timeout
+TIMEOUT = 100  # In seconds
+MAX_ATTEMPTS = 12  # Number of Retries upon timeout
 MP_COUNT = 4  # How many parallel processes
 
 
 # %% Selector:
-def plate_selector(plate_amount, plate_numbers, csv_folder):
+def plate_selector(plate_amount, plate_numbers, extract_folder, csv_folder, mt_folder):
     """
     Connects to the ftp server and return the list of plates' files
     according to the input parameters
@@ -55,21 +55,39 @@ def plate_selector(plate_amount, plate_numbers, csv_folder):
     if not ftp:
         exit(-1)
 
-    plate_list = [plate for plate in ftp.nlst() if pattern.fullmatch(plate)]
+    plate_list = {plate for plate in ftp.nlst() if pattern.fullmatch(plate)}
 
-    if path.exists(csv_folder):
-        csv_files = [f.name for f in scandir(csv_folder) if f.is_file and f.name.endswith(".csv")]
-        csv_numbers = [f.split('.')[0] for f in csv_files]
-        csv_plate_files = [f'Plate_{f}.tar.gz' for f in csv_numbers]
-        plate_list = [plate for plate in plate_list if plate not in csv_plate_files]
-        dont_download = [plate for plate in plate_list if plate in csv_plate_files]
+    def get_raw_plate_files_names(folder):
+        if path.exists(folder):
+            files = {f.name for f in scandir(folder) if f.is_file and f.name.endswith(".csv")}
+            numbers = {f.split('.')[0] for f in files}
+            return {f'Plate_{f}.tar.gz' for f in numbers}
+
+        return set()
+
+    csv_plate_files, mt_plate_files = [get_raw_plate_files_names(fld) for fld in [csv_folder, mt_folder]]
+    plate_files = csv_plate_files.intersection(mt_plate_files)
+    dont_download = {plate for plate in plate_list if plate in plate_files}
+    plate_list = {plate for plate in plate_list if plate not in plate_files}
+    if len(dont_download) > 0:
+        print(f'Notice: {dont_download} already have refined files')
+
+    download_list = plate_list.copy()
+
+    if path.exists(extract_folder):
+        ext_flds = {f for f in listdir(extract_folder) if len(listdir(path.join(extract_folder, f))) == 2}
+        ext_numbers = {f.split('_')[1] for f in ext_flds}
+        ext_plate_files = {f'Plate_{f}.tar.gz' for f in ext_numbers}
+        dont_download = {plate for plate in download_list if plate in ext_plate_files}
+        download_list = {plate for plate in download_list if plate not in ext_plate_files}
         if len(dont_download) > 0:
-            print(f'Notice: {dont_download} already have refined csv files')
+            print(f'Notice: {dont_download} already have extracted files')
 
     if plate_amount and plate_amount < len(plate_list):
         plate_list = sample(plate_list, plate_amount)
+        download_list = download_list.intersection(plate_list)
 
-    return ftp, plate_list
+    return ftp, plate_list, download_list
 
 
 # %% Downloader:
@@ -187,6 +205,8 @@ def extractor_file(plate_file, destination):
     plate_number = plate_name.split("_")[1]
 
     curr_dest = path.join(destination, plate_name)
+    if path.exists(curr_dest):
+        print(f'{plate_name} already extracted, skipping...')
 
     sql_file = r"gigascience_upload/{}/extracted_features/{}.sqlite".format(plate_name, plate_number)
     profile_file = r"gigascience_upload/{}/profiles/mean_well_profiles.csv".format(plate_name)
@@ -205,6 +225,7 @@ def extractor_file(plate_file, destination):
 
         tar.close()
         del tar
+        remove(plate_file)
 
     except:
         print(f'Warning: {plate_name}.tar.gz corrupted, deleting file...')
@@ -238,65 +259,100 @@ def extractor(tars_dir, plate_list, destination):
 
 
 # %% Merger:
-def merger(directory, plate_folders, destination):
-    makedirs(destination, exist_ok=True)
+def merger(directory, plate_folders, destinations):
+    for destination in destinations:
+        makedirs(destination, exist_ok=True)
+
     dir_list = [f.name for f in scandir(directory) if f.is_dir()]
     dir_list = [fld for fld in dir_list if fld in plate_folders]
 
     p = Pool(MP_COUNT)
-    p_bar = tqdm(desc="Merging... ", total=len(dir_list))
+    print("Merging... ")
 
-    def success(result):
-        nonlocal p_bar
-        p_bar.update()
-
-    p.starmap_async(merge_plate, zip(cycle([destination]), cycle([directory]), dir_list), callback=success)
+    p.starmap(merge_plate, zip(cycle([destinations]), cycle([directory]), dir_list))
 
     p.close()
     p.join()
 
 
-def merge_plate(destination, directory, plate_folder):
+def merge_plate(destinations, directory, plate_folder):
+    csv_dest , mt_dest = destinations
     print("Merging {}".format(plate_folder))
     folder_path = path.join(directory, plate_folder)
     plate_number = plate_folder.split('_')[1]
-    output = path.join(destination, plate_number + ".csv")
-    if path.lexists(output):
-        print("Warning: {} already merged, skipping...".format(plate_folder))
-        # continue
+
     sql_file = path.join(folder_path, plate_number + ".sqlite")
     well_file = path.join(folder_path, "mean_well_profiles.csv")
-    df_well = pd.read_csv(well_file,
-                          index_col="Metadata_Well",
-                          usecols=["Metadata_Well", "Metadata_ASSAY_WELL_ROLE", "Metadata_broad_sample"])
-    con = sqlite3.connect(sql_file)
-    query = "SELECT Cells.*, Image.Image_Metadata_Well FROM Cells " \
-            "INNER JOIN Image ON Cells.ImageNumber = Image.ImageNumber"
-    df_cells = pd.read_sql_query(query, con)
-    con.close()
-    df_join = df_cells.join(df_well, "Image_Metadata_Well", "inner")
 
-    df_join.rename(columns={"TableNumber": "Plate"}, inplace=True)
-    df_join["Plate"] = plate_folder.split('_')[1]
+    csv_output = path.join(csv_dest, plate_number + ".csv")
+    mt_output = path.join(mt_dest, plate_number + ".csv")
+    if path.lexists(csv_output):
+        print("Warning: {} already have csv file, skipping...".format(plate_folder))
+    else:
+        df_well = pd.read_csv(well_file,
+                              index_col="Metadata_Well",
+                              usecols=["Metadata_Well", "Metadata_ASSAY_WELL_ROLE", "Metadata_broad_sample"])
+        con = sqlite3.connect(sql_file)
+        query = "SELECT Cells.*, Image.Image_Metadata_Well FROM Cells " \
+                "INNER JOIN Image ON Cells.ImageNumber = Image.ImageNumber"
+        df_cells = pd.read_sql_query(query, con)
+        con.close()
+        df_join = df_cells.join(df_well, "Image_Metadata_Well", "inner")
 
-    df_join.to_csv(output, index=False)
-    del df_well, df_cells, df_join
+        df_join.rename(columns={"TableNumber": "Plate"}, inplace=True)
+        df_join["Plate"] = plate_folder.split('_')[1]
+
+        df_join.to_csv(csv_output, index=False)
+        del df_well, df_cells, df_join
+
+    if path.lexists(mt_output):
+        print("Warning: {} already have metadata file, skipping...".format(plate_folder))
+    else:
+        df_well = pd.read_csv(well_file,
+                              index_col="Metadata_Well",
+                              usecols=["Metadata_Well", "Metadata_ASSAY_WELL_ROLE", "Metadata_broad_sample"])
+        sql_file = path.join(folder_path, plate_number + ".sqlite")
+        con = sqlite3.connect(sql_file)
+        query = "SELECT Image_Metadata_Plate,Image_Metadata_Well,Image_Metadata_Site,ImageNumber," + \
+                ','.join([f'Image_FileName_Orig{c}' for c in ['AGP', 'DNA', 'ER', 'Mito', 'RNA']]) + \
+                " FROM Image"
+        df_imgs = pd.read_sql_query(query, con)
+        con.close()
+    
+        df_imgs = df_imgs.join(df_well, 'Image_Metadata_Well', 'inner')
+
+        rename_dict = {
+            **{f'Image_FileName_Orig{c}': c for c in ['AGP', 'DNA', 'ER', 'Mito', 'RNA']},
+            **{f'Image_Metadata_{t}': t for t in ['Plate', 'Well', 'Site']},
+            'Metadata_ASSAY_WELL_ROLE': 'Well_Role',
+            'Metadata_broad_sample': 'Broad_Sample'
+        }
+        df_imgs.rename(rename_dict, axis=1, inplace=True)
+
+        df_imgs.set_index(['Plate', 'Well', 'Site', 'ImageNumber', 'Well_Role', 'Broad_Sample'], inplace=True)
+
+        df_imgs.to_csv(mt_output)
+        del df_well
+    
+    remove(well_file)
+    remove(sql_file)
+    remove(folder_path)
 
 
 # %% Main function:
 def main(working_path, plate_amount, plate_numbers):
     download_path = path.join(working_path, "tars")
     extract_path = path.join(working_path, "extracted")
-    merge_path = path.join(working_path, "csvs")
+    merge_paths = path.join(working_path, "csvs"), path.join(working_path, "metadata")
 
-    ftp, plate_list = plate_selector(plate_amount, plate_numbers, merge_path)
+    ftp, plate_list, download_list = plate_selector(plate_amount, plate_numbers, extract_path, *merge_paths)
 
-    download_plates(ftp, download_path, plate_list)
+    download_plates(ftp, download_path, download_list)
 
-    extractor(download_path, plate_list, extract_path)
+    extractor(download_path, download_list, extract_path)
 
     plate_folders = [plate.split('.')[0] for plate in plate_list]
-    merger(extract_path, plate_folders, merge_path)
+    merger(extract_path, plate_folders, merge_paths)
 
 
 def valid_numbers(str_list):
