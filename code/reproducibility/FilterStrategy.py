44774cd0b7368fd37997ae0238f7be600f1a5f7b
Naorko <Naorko@post.bgu.ac.il>
2022-01-23 09:45:11 +0200

    Sync remote

--- code/reproducibility/FilterStrategy.py
+++ code/reproducibility/FilterStrategy.py
@@ -9,6 +9,10 @@ class FilterStrategy(ABC):
     def filter_dataframe(self, compounds: pd.DataFrame, field: str):
         pass
 
+    @abstractmethod
+    def get_filter_threshold(self) -> float:
+        pass
+
 
 class AbsoluteFilterStrategy(FilterStrategy):
     def __init__(self, filter_threshold):
@@ -23,11 +27,49 @@ class AbsoluteFilterStrategy(FilterStrategy):
         comp_names = counts.query('zero > 1 and filter > 0').index
         return compounds[compounds.index.isin(comp_names, 1)]
 
+    def get_filter_threshold(self) -> float:
+        return self._filter_threshold
+
 
 class TopKFilterStrategy(FilterStrategy):
     def __init__(self, top_k):
         self._top_k = top_k
+        self._filter_threshold = None
 
     def filter_dataframe(self, compounds: pd.DataFrame, field: str):
         # return top_k compounds
-        return compounds.nlargest(self._top_k, field)
+        topk_df = compounds.nlargest(self._top_k, field)
+        self._filter_threshold = topk_df[field].min()
+        return topk_df
+
+    def get_filter_threshold(self) -> float:
+        return self._filter_threshold
+
+
+class TopKFromDupFilterStrategy(FilterStrategy):
+    def __init__(self, top_k):
+        self._top_k = top_k
+        self._filter_threshold = None
+
+    def filter_dataframe(self, compounds: pd.DataFrame, field: str):
+        # return top_k compounds
+        compounds_cnt = compounds.query(f'{field} >= 0').index.get_level_values(1).value_counts()
+        dup_compounds = (compounds_cnt[compounds_cnt > 1]).index
+        topk_df = compounds[compounds.index.isin(dup_compounds, 1)].nlargest(self._top_k, field)
+        self._filter_threshold = topk_df[field].min()
+        return topk_df
+
+    def get_filter_threshold(self) -> float:
+        return self._filter_threshold
+
+
+class TopKFromDupWithMatchFilterStrategy(TopKFromDupFilterStrategy):
+    def __init__(self, top_k):
+        super().__init__(top_k)
+
+    def filter_dataframe(self, compounds: pd.DataFrame, field: str):
+        # return top_k compounds
+        topk_df = super().filter_dataframe(compounds, field)
+        comp_names = topk_df.index.get_level_values(1).unique()
+        topk_df = compounds[compounds.index.isin(comp_names, 1)]
+        return topk_df
